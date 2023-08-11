894db09c6425fb4b0896c6b6a13a9a07f021cad4
covenantcodes <covenantifeoluwa4@gmail.com>
2023-08-11 11:26:21 +0100

    added Login Screen

---
+++ Screens/LoginScreen.js
@@ -0,0 +1,37 @@
+import React, { useState } from 'react';
+import { View, Text, TextInput, Button } from 'react-native';
+import { useUser } from './UserContext';
+
+const LoginScreen = () => {
+  const { setUser } = useUser();
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleLogin = () => {
+    // Perform login validation logic here
+    // For simplicity, let's assume successful login
+    setUser({ username });
+  };
+
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text style={{ marginBottom: 20, fontSize: 18 }}>Login</Text>
+      <TextInput
+        placeholder="Username"
+        style={{ marginBottom: 10, padding: 10, borderWidth: 1, width: 200 }}
+        value={username}
+        onChangeText={setUsername}
+      />
+      <TextInput
+        placeholder="Password"
+        secureTextEntry
+        style={{ marginBottom: 20, padding: 10, borderWidth: 1, width: 200 }}
+        value={password}
+        onChangeText={setPassword}
+      />
+      <Button title="Login" onPress={handleLogin} />
+    </View>
+  );
+}
+
+export default LoginScreen;
